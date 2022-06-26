1a52f2a6e55d6e87e23da6c4392916ed2cc0bb0a
yusuke.sakurai <39reyskhky@gmail.com>
2022-06-26 13:12:32 +0900

    gin controllers

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
+	controller "golang-app/app/controllers"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World!",
-		})
-	})
+	r.GET("/hello", controller.Hello)
 	r.Run(":8080")
 }
